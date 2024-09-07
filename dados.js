615818dde341142abfc8d8d8de39b240adaa5199
bella210795 <bella210795@hotmail.com>
2024-09-06 21:17:36 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+        titulo : "Big Bang",
+        descricao : "O Big Bang, um dos grupos mais influentes da história do K-pop, revolucionou a indústria musical sul-coreana. Formado pela YG Entertainment em 2006, o quinteto conquistou o mundo com seu som único, mesclando hip-hop, R&B e pop. Com hits memoráveis como Fantastic Baby e Bang Bang Bang, o Big Bang se tornou um fenômeno global, quebrando recordes e inspirando gerações de artistas. Apesar de hiatos e mudanças na formação, o legado do Big Bang permanece inabalável, consolidando seu lugar como um dos maiores grupos de K-pop de todos os tempos.",
+        link : "https://pt.wikipedia.org/wiki/Big_Bang_(grupo_musical)",
+        tags : "revolucionou fantastic baby"
+    },
+    {
+        titulo: "4Minute",
+        descricao: "O 4Minute foi um grupo feminino sul-coreano conhecido por suas performances poderosas e conceito único. Debutaram em 2009 sob a Cube Entertainment e marcaram época com hits como 'Hot Issue' e 'Muzik'. O grupo se destacou por sua versatilidade, explorando diferentes estilos musicais e quebrando estereótipos do K-pop.",
+        link: "https://pt.wikipedia.org/wiki/4Minute",
+        tags : "feminino muzik"
+      },
+      {
+        titulo: "Super Junior",
+        descricao: "O Super Junior é um dos grupos masculinos mais populares e longevos do K-pop. Formado pela SM Entertainment em 2005, o grupo se tornou conhecido por sua grande quantidade de membros e por suas músicas cativantes. Hits como 'Sorry, Sorry' e 'Mr. Simple' os consagraram como um dos maiores nomes da indústria.",
+        link: "https://pt.wikipedia.org/wiki/Super_Junior",
+        tags : "sorry sm maiores"
+      },
+      {
+        titulo: "TVXQ!",
+        descricao: "O TVXQ!, também conhecido como DBSK, é um dos grupos mais influentes da primeira geração do K-pop. Formado em 2003 pela SM Entertainment, o grupo se destacou por suas performances sincronizadas e vocais poderosos. Hits como 'Mirotic' e 'Keep Your Head Down' os tornaram um fenômeno na Ásia.",
+        link: "https://pt.wikipedia.org/wiki/TVXQ!",
+        tags : "DBSK primeira geração"
+      },
+      {
+        titulo: "KARA",
+        descricao: "O KARA foi um dos primeiros grupos femininos sul-coreanos a conquistar sucesso no Japão. Formado pela DSP Media em 2007, o grupo se destacou por suas músicas alegres e coreografias cativantes. Hits como 'Mister' e 'Pretty Girl' as tornaram um ícone do K-pop.",
+        link: "https://pt.wikipedia.org/wiki.KARA_(grupo_musical)",
+        tags : "dsp japão mister"
+      },
+      {
+        titulo: "SHINee",
+        descricao: "O SHINee é um grupo masculino conhecido por sua música inovadora e performances sincronizadas. Formado pela SM Entertainment em 2008, o grupo se destacou por explorar diferentes gêneros musicais, como R&B e eletrônico. Hits como 'Replay' e 'Ring Ding Dong' os tornaram um dos grupos mais populares da sua geração.",
+        descrição: "https://pt.wikipedia.org/wiki/SHINee",
+        tags : "replay quinteto"
+      },
+      {
+        titulo: "Girls' Generation",
+        descricao: "As Girls' Generation, também conhecidas como SNSD, são um dos grupos femininos mais populares e influentes da história do K-pop. Formado pela SM Entertainment em 2007, o grupo se tornou conhecido por suas músicas cativantes e visuais impecáveis. Hits como 'Gee' e 'I Got a Boy' as consagraram como um dos maiores nomes da indústria.",
+        link: "https://pt.wikipedia.org/wiki/Girls%27_Generation",
+        tags : "SNSD cativantes"
+      }
+    ];
